d2ada7d593ecb24b538cb0f1d75bf33e788c92a1
Juvan-Thavalingam <100355516+Juvan-Thavalingam@users.noreply.github.com>
2023-12-18 18:54:20 +0100

    Add files via upload

---
+++ 03_Comparable_And_Comparator/src/ch/zhaw/ads/RankingListServer.java
@@ -0,0 +1,43 @@
+package ch.zhaw.ads;
+
+import java.util.*;
+
+public class RankingListServer implements CommandExecutor {
+
+    public List<Competitor> createList(String rankingText) {
+        List<Competitor> competitorList = new LinkedList<>();
+        String[] lines = rankingText.split("\n");
+        for (String line : lines) {
+            String name = line.split(";")[0];
+            String time = line.split(";")[1];
+            competitorList.add(new Competitor(0, name,  time));
+        }
+        return competitorList;
+    }
+
+    public String createSortedText(List<Competitor> competitorList) {
+        Collections.sort(competitorList);
+        int rank = 1;
+        StringBuilder sb = new StringBuilder();
+        for (Competitor c : competitorList) {
+            c.setRank(rank++);
+            sb.append(c).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String createNameList(List<Competitor> competitorList) {
+        Collections.sort(competitorList, new AlphaComparatorCompetitor());
+
+        StringBuilder sb = new StringBuilder();
+        for (Competitor c : competitorList) {
+            sb.append(c).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String execute(String rankingList) {
+        List<Competitor> competitorList = createList(rankingList);
+        return "Rangliste\n" + createSortedText(competitorList);
+    }
+}
